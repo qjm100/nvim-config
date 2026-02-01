9d61d54d37da8b1c46b26681203d11fadd8279bf
qjm100 <1723687845@qq.com>
2026-02-01 15:28:20 +0800

    make it more beauty

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,21 +1,41 @@
+---@class snacks.dashboard.Config
 return {
-  "folke/snacks.nvim",
-
-  priority = 1000,
-  lazy = false,
-  ---@type snacks.Config
-  opts = {
-    dashboard = {
-      preset = {
-        header = [[
+  {
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+      dashboard = {
+        -- 基础配置将在这里添加
+        preset = {
+          -- Defaults to a picker that supports `fzf-lua`, `telescope.nvim` and `mini.pick`
+          pick = nil,
+          -- Used by the `keys` section to show keymaps.
+          -- Set your custom keymaps here.
+          -- When using a function, the `items` argument are the default keymaps.
+          -- Used by the `header` section
+          header = [[
  ██████╗███╗   ██╗███╗   ███╗    ██████╗ ███╗   ██╗██╗   ██╗
 ██╔════╝████╗  ██║████╗ ████║   ██╔════╝ ████╗  ██║██║   ██║
 ██║     ██╔██╗ ██║██╔████╔██║   ██║  ███╗██╔██╗ ██║██║   ██║
 ██║     ██║╚██╗██║██║╚██╔╝██║   ██║   ██║██║╚██╗██║██║   ██║
 ╚██████╗██║ ╚████║██║ ╚═╝ ██║▄█╗╚██████╔╝██║ ╚████║╚██████╔╝
  ╚═════╝╚═╝  ╚═══╝╚═╝     ╚═╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝ 
-                                                            
-       ]],
+
+]],
+        },
+        sections = {
+          { section = "header" },
+          { section = "keys", gap = 1, padding = 1 },
+          { section = "startup" },
+          {
+            section = "terminal",
+            cmd = "pokemon-colorscripts -r --no-title; sleep .1",
+            random = 10,
+            pane = 2,
+            indent = 4,
+            height = 30,
+          },
+        },
       },
     },
   },
